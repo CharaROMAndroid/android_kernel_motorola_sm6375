cc6872f2aacb506ee76bc4f9c86b8ae509f9ae00
bunnypad <bunnypad@murena.io>
2026-08-15 21:33:55 -0400

    ip6_tunnel: fix C89 declaration-after-statement warnings
    Move nh variable declaration to function start to comply with
    C89 standard (no mixed declarations and code).
    
    Remove unused nh assignment in ip6_tnl_xmit.
    
    Change-Id: I0f099d5c5e5321563498bdc079e6bfba927772fc

--- net/ipv6/ip6_tunnel.c
+++ net/ipv6/ip6_tunnel.c
@@ -782,6 +782,7 @@ static int __ip6_tnl_rcv(struct ip6_tnl *tunnel, struct sk_buff *skb,
 	struct pcpu_sw_netstats *tstats;
 	const struct ipv6hdr *ipv6h = ipv6_hdr(skb);
 	int err;
+	int nh;
 
 	if ((!(tpi->flags & TUNNEL_CSUM) &&
 	     (tunnel->parms.i_flags & TUNNEL_CSUM)) ||
@@ -820,7 +821,7 @@ static int __ip6_tnl_rcv(struct ip6_tnl *tunnel, struct sk_buff *skb,
 		skb->dev = tunnel->dev;
 	}
 
-	int nh = skb_network_offset(skb);
+	nh = skb_network_offset(skb);
 	skb_reset_network_header(skb);
 
 	if (!skb_vlan_inet_prepare(skb)) {
@@ -1229,7 +1230,6 @@ route_lookup:
 	}
 
 	skb_push(skb, sizeof(struct ipv6hdr));
-	int nh = skb_network_offset(skb);
 	skb_reset_network_header(skb);
 	ipv6h = ipv6_hdr(skb);
 	ip6_flow_hdr(ipv6h, dsfield,
